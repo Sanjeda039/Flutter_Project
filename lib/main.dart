d254e9725f96d5913f6550edbf58f09bdad33053
Sanjeda039 <109533731+Sanjeda039@users.noreply.github.com>
2024-02-14 12:31:14 +0600

    Row feature

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,26 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.purple[600],
       ),
-      body: Center(
-        child: IconButton(
-          onPressed: () {
-          print('Oooops you pressed me!');
-          },
-          icon: Icon(
-          Icons.mail
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Hello World'),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.teal, // Set background color
+              foregroundColor: Colors.white, // Set text color
+            ),
+            onPressed: () { },
+            child: Text('Click me'),
           ),
-          ),
-          ),
+          Container(
+            color: Colors.blueGrey,
+            padding: EdgeInsets.all(30.0),
+            child: Text ('inside container'),
+          )
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () { },
         backgroundColor: Colors.purple[600],
